5ea2620422944977d79b10760ba316b873a45107
xuanbinh313 <xuanbinh313@gmail.com>
2023-09-17 23:43:51 +0700

    create search

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -81,7 +81,7 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 {
                     isModerator && (
                         <DropdownMenuItem
-
+                            onClick={() => onOpen("createChannel")}
                             className='px-3 py-2 text-sm cursor-pointer'
                         >
                             Create Channel
@@ -94,6 +94,7 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 {
                     isAdmin && (
                         <DropdownMenuItem
+                            onClick={() => onOpen("deleteServer",{server})}
                             className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
                         >
                             Delete Server
@@ -105,6 +106,7 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 {
                     !isAdmin && (
                         <DropdownMenuItem
+                            onClick={() => onOpen("leaveServer", { server })}
                             className='px-3 py-2 text-sm cursor-pointer'
                         >
                             Leave Server
